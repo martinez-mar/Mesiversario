73825eeb5e026dde536931463456e2dbc613df49
Martha <mtzmarthaa23@gmail.com>
2026-06-12 21:38:06 -0600

    ahora chi

--- Mesiversario/config.js
+++ Mesiversario/config.js
@@ -1,51 +1,42 @@
 // mesiversario 30, aprendiendo github y progra a la vez xd
 //Titulo inicial e icono de la app
+
 const config = {
     pageTitle: "Feliz mesiversario ❤️",
     favicon: "assets/cabezon.jpeg",
-//Musica de fondo
+
+    // Musica de fondo
     music: {
         path: "thismagic.mp3",
         volume: 0.3,
         autoPlay: true
     },
-//Mensajes personalizados
+
+    // Mensajes personalizados
     loading: {
-        // Mensaje que aparece en la pantalla de inicio
         message: "¿Preparado?... tu novia es genial B)",
-
-        // Texto del botón para comenzar
         buttonText: "iendooooo",
-
-        // Texto de ayuda que aparece arriba del botón
         clickHint: "Pushale aki",
     },
 
     hero: {
-        // Título principal que aparece al inicio
         title: "Nuestra historia juntos...",
-        //Fecha de aniversario
         startDate: "2023-12-13",
 
-        // Texto que aparece después del contador
         finalText: "Aquí andamos pq aquí es en donde nos puso la vidaaaaaaaa uwu",
 
-        // Texto del indicador de scroll
         scrollText: "Jalele pa ver más 7u7",
     },
 
     timeline: [
-        // ========== EVENTO 1 ==========
         {
-            // Título del evento (puedes usar emojis)
             title: "La perfección hecha hombre 😍",
 
-            // Descripción del evento (puedes usar HTML como <br> para saltos de línea)
             content: `Empecemos recordándote lo mucho que me encantas, no solo físicamente, pero diosssss, mira a semejante rubioooo OwO`,
-           
+
             images: [
                 "assets/dorado.jpeg",
-                "assets/parqueabrazo.jpeg",
+                "assets/parqueabrazo.jpeg"
             ],
 
             footer: "Soy la mujer más afortunada del mundo por tenerte a mi lado ❤️u❤️",
@@ -53,39 +44,52 @@ const config = {
 
         {
             title: "Soy tan feliz a tu lado 🥰",
+
             content: `Complementas mis días y me divierto mucho contigo, no puedo esperar por el día en que ya no tengamos que separarnos :,)`,
-            images: ["assets/luzroja2.jpeg"],
-            images: ["assets/luzroja1.jpeg"],
-            images: ["assets/luzroja3.jpeg"],
-            images: ["assets/plaza2.jpeg"],
+
+            images: [
+                "assets/luzroja2.jpeg",
+                "assets/luzroja1.jpeg",
+                "assets/luzroja3.jpeg",
+                "assets/plaza2.jpeg"
+            ],
+
             footer: "Cada momento contigo es un tesoro que guardo en mi mente, en mi corazón ❤️",
         },
 
-        // ========== EVENTO 3 ==========
         {
             title: "Gracias por tantas aventuras y momentos increíbles, y por carrearte cuando lo necesitas jsaajsja :3",
-            content: `Bueno, tengo que admitir que el proplayer eres tú<3 me excita como juegas, digo, me encanta jsjsjs, eres muy sexy haciendo cualquier cosa 7u7`,
-            images: ["assets/ajedrez.jpeg"],
-            images: ["assets/ajedrez3.jpeg"],
-            images: ["assets/abasho.jpeg"],
-            images: ["assets/ajedrez2.jpeg"],
+
+            content: `Bueno, tengo que admitir que el proplayer eres tú<3 me encanta jsjsjs, eres muy sexy haciendo cualquier cosa 7u7`,
+
+            images: [
+                "assets/ajedrez.jpeg",
+                "assets/ajedrez3.jpeg",
+                "assets/abasho.jpeg",
+                "assets/ajedrez2.jpeg"
+            ],
+
             footer: "Nervios, risas y mariposas en el estómago, miedo, envidia, todo eso que yo te provoco ;) ntc JAJA tú tambn a mi pa que no llores :3",
         },
-        // ========== EVENTO 4 ==========
+
         {
             title: "Gracias por procurarme, por hacerme sentir amada",
+
             content: `De verdad que qué bonito fue coincidir contigo y por fin aprender lo que es el amor, que te aman y que eres suficiente, gracias por salvarme s2`,
-            images: ["assets/baño.jpeg"],
-            images: ["assets/casasi.jpeg"],
-            images: ["assets/casasi2.jpeg"],
+
+            images: [
+                "assets/baño.jpeg",
+                "assets/casasi.jpeg",
+                "assets/casasi2.jpeg"
+            ],
+
             footer: "Gracias por estos 30 meses damsito lindo, no hay dinero ni berries pero hay manitas programadoras y sin envidias paros jajajasju❤️",
-        },
+        }
+    ],
 
     gallery: {
-        // Título de la sección de galería
         title: "Recuerdos más recientes de navidad, mi visita favorita nwn",
 
-        // Lista de fotos para la galería
         images: [
             "assets/gloss.jpeg",
             "assets/luces.jpeg",
@@ -102,13 +106,11 @@ const config = {
             "assets/sanignacio.jpeg",
             "assets/walmart.jpeg",
             "assets/walmart2.jpeg",
-            "assets/cabezon.jpeg",
+            "assets/cabezon.jpeg"
         ]
     },
 
     finalMessage: {
-        // Mensaje de cierre (puedes usar <br> para saltos de línea)
-        content: "Gracias por ser parte de mi vida ❤️<br>Gracias por estos 30 increibles meses :3 <br>Gracias por tenerme envidia para hacerme más fuerte XD <br>Te amo infinitamente mi guerito lindo, amor de mi vida, mi todo S2",
+        content: "Gracias por ser parte de mi vida ❤️<br>Gracias por estos 30 increíbles meses :3<br>Gracias por tenerme envidia para hacerme más fuerte XD<br>Te amo infinitamente mi guerito lindo, amor de mi vida, mi todo S2",
     }
 };
-
